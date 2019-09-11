a3b7e421e69fc045221cd3e87dc609c2fd70217f
chrisbmoore <chrisbemore@gmail.com>
2019-09-11 15:05:38 -0400

    Find nearest clusters and update memberships.

--- app/k-means-generator.ts
+++ app/k-means-generator.ts
@@ -1,6 +1,7 @@
 import { Membership } from "./membership";
 import { Point } from "./point";
 import { Cluster } from "./cluster";
+import { euclidean } from 'ml-distance-euclidean';
 
 export class KMeansGenerator {
     private _k: number;
@@ -16,6 +17,7 @@ export class KMeansGenerator {
         // Calculate dimensions from Points.
         this._dimensions = this._points.length ? this._points[0].coordinates.length : 0;
         this._clusters = this.initializeClusters(k, this._points);
+        this.updateMemberships(this._points, this._clusters); //TODO reassign _memberships
     }
 
     private convertDataToPoints(data: number[][]): Point[] {
@@ -40,5 +42,28 @@ export class KMeansGenerator {
             return point;
         });
         return centroids;
+    }
+
+    private updateMemberships(points: Point[], clusters: Cluster[]) { //return copy of memberships
+        this._memberships = [];
+        points.forEach(point => {
+            // For each point, find the nearest Cluster centroid.
+            const nearestCluster = this.nearestCluster(point, clusters);
+            // Change the point to be a full member of that cluster.
+            this._memberships.push(new Membership(point, nearestCluster, 1));
+        });
+    }
+
+    private nearestCluster(point: Point, clusters: Cluster[]): Cluster {
+        let nearestCluster: Cluster;
+        let nearestDistance = Number.MAX_VALUE;
+        clusters.forEach(cluster => {
+            const distance = euclidean(point.coordinates, cluster.centroid.coordinates);
+            if (distance < nearestDistance) {
+                nearestDistance = distance;
+                nearestCluster = cluster;
+            }
+        });
+        return nearestCluster;
     }
 }
--- app/membership.ts
+++ app/membership.ts
@@ -2,6 +2,11 @@ import { Point } from "./point";
 import { Cluster } from "./cluster";
 
 export class Membership {
+    constructor(point: Point, cluster: Cluster, ratio: number) {
+        this.point = point;
+        this.cluster = cluster;
+        this.ratio = ratio;
+    }
     point: Point;
     cluster: Cluster;
     ratio: number;
